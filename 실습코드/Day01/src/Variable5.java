2920ceaaee2ceb146befd6e6c8f8a0929a0f42e4
LeeSoeun <soeun318@naver.com>
2024-11-12 23:47:30 +0900

    Day 01 시범 코드

--- 실습코드/Day01/src/Variable5.java
+++ 실습코드/Day01/src/Variable5.java
@@ -6,6 +6,11 @@ public class Variable5 {
         // 수정 코드 작성 시작
         a = 456;
         b = 123;
+
+        // 변수만 사용하면 이렇게 작성
+        int c = a;
+        a = b;
+        b = c;
         // 수정 코드 작성 끝
 
         System.out.println("a :" + a);
