aa0827a19040251506e116a8105130d95f5a00ec
LeeSoeun <soeun318@naver.com>
2024-11-14 18:32:52 +0900

    Day02 실습 코드

---
+++ 실습코드/Day02/src/Comparison2.java
@@ -0,0 +1,10 @@
+// 비교 연산자 예제 2
+public class Comparison2 {
+    public static void main(String[] args) {
+        System.out.println(10 >= 3);
+        System.out.println(10 < 5);
+        System.out.println(10 == 7);
+        System.out.println(5 <= 3);
+        System.out.println(1 != 1);
+    }
+}
