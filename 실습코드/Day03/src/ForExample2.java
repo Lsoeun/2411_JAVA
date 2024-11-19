7f3f1dd41a82b379160722d7ed5c656f4c9c0e59
LeeSoeun <soeun318@naver.com>
2024-11-19 13:32:50 +0900

    Day03 실습 코드

--- 실습코드/Day03/src/ForExample2.java
+++ 실습코드/Day03/src/ForExample2.java
@@ -1,9 +1,9 @@
 public class ForExample2 {
     public static void main(String[] args) {
-        int i;
-        int sum;
-        for (i = 1, sum = 0; i <= 10; i++) {
-            sum += i;
+        int i; // 반복 횟수와 관련된 i
+        int sum = 0; // 합계가 누적될 값을 담을 sum
+        for (i = 1; i <= 10; i++) {
+            sum += i; // sum = sum + i
         }
 
         System.out.println("1부터 10까지의 합은 " + sum + "입니다.");
