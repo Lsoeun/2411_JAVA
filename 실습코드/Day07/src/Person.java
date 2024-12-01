fad5c16614ddddb245bbd37c57b995829a1acc43
LeeSoeun <soeun318@naver.com>
2024-12-01 15:13:03 +0900

    Day07 수업코드

--- 실습코드/Day07/src/Person.java
+++ 실습코드/Day07/src/Person.java
@@ -10,12 +10,12 @@ public class Person {
         this.age = age;   // 필드 age 초기화
     }
     public static void main(String[] args) {
-        // 생성자가 없는 경우 (비효율적)
+        // 매개변수가 있는 생성자가 없는 경우 (비효율적)
         // Person person = new Person();
         // person.name = "홍길동";
         // person.age = 25;
 
-        // 생성자를 사용하는 경우
+        // 매개변수가 있는 생성자를 사용하는 경우(생성자를 통한 초기화)
         Person person = new Person("홍길동", 25); // 간단하고 직관적
         System.out.println(person.name); // 출력: 홍길동
         System.out.println(person.age);  // 출력: 25
--- 실습코드/Day07/src/Person2.java
+++ 실습코드/Day07/src/Person2.java
@@ -7,8 +7,8 @@ public class Person2 {
      * 사람 이름을 매개변수로 입력받아서
      * Person 클래스를 생성하는 생성자
      */
-    public Person2(String pname) {
-        name = pname;
+    public Person2(String name) {
+        this.name = name;
     }
 
     public Person2(String name, float height, float weight) {
