7022d424ca8b8486f1351b1f4c167ea2aa4521c4
LeeSoeun <soeun318@naver.com>
2024-12-09 13:40:20 +0900

    Day11 실습 코드

--- 실습코드/Day11/src/Customer.java
+++ 실습코드/Day11/src/Customer.java
@@ -10,4 +10,10 @@ public class Customer implements Buy, Sell {
     public void buy() {
         System.out.println("구매하기");
     }
+
+    // 디폴트 메서드 order()를 Customer 클래스에서 재정의함
+    @Override
+    public void order() {
+        System.out.println("고객 판매 주문");
+    }
 }
