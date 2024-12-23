8784bcd43dbaa1c8c2648d05b35c60e364a8d189
LeeSoeun <soeun318@naver.com>
2024-12-23 15:03:51 +0900

    Day16 수업자료

--- 실습코드/Day16/src/ArrayListEx2.java
+++ 실습코드/Day16/src/ArrayListEx2.java
@@ -18,6 +18,22 @@ public class ArrayListEx2 {
         // 숫자 타입의 데이터는 인덱스와 구별을 위해 객체를 선언하여 삭제
         list.remove(Integer.valueOf(10));
 
-        System.out.println("리스트 값: " + list);
+        System.out.println("삭제 후 리스트 값: " + list);
+
+        List<String> list2 = new ArrayList<>();
+
+        // 문자열 추가
+        list2.add("Apple");
+        list2.add("Banana");
+        list2.add("Cherry");
+        System.out.println("리스트 값: " + list2);
+
+        // 특정 인덱스의 값 삭제
+        list2.remove(2); // 2번째 인덱스 삭제 ("Cherry")
+        System.out.println("2번째 인덱스 삭제 후: " + list2);
+
+        // 특정 값을 삭제
+        list2.remove("Banana"); // "Banana" 삭제
+        System.out.println("특정 값 삭제 후: " + list2);
     }
 }
